d71321e81d13bfeb14a40dd0353a442643495e97
Ubuntu <ubuntu@ip-172-31-91-211.ec2.internal>
2019-11-21 14:28:21 +0000

    create awesome platformer game

---
+++ projects/platformer/js/init/platform.js
@@ -0,0 +1,49 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.platform = window.opspark.platform || {};
+    
+    let platform = window.opspark.platform;
+    
+    /**
+     * init: This function initializes the platforms for the level.
+     * 
+     * GOAL: Add as many platforms necessary to make your level challenging.
+     * 
+     * Use the createPlatform Function to create platforms for the level. 
+     * 
+     * createPlatform() takes these arguments:
+     *      
+     *      createPlatform(x, y, scaleX, scaleY);
+     * 
+     *      x: The x coordineate for the platform.
+     *      y: The y coordineate for the platform.
+     *      scaleX: OPTIONAL The scale factor on the x-axis, this value will 
+     *              stretch the platform in width.
+     *      scaleY: OPTIONAL The scale factor on the y-axis, this value will 
+     *              stretch the platform in height.
+     */ 
+    function init(game) {
+        let createPlatform = platform.create;
+
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        /*
+         * ground : here, we create a floor. Given the width of of the platform 
+         * asset, giving it a scaleX and scaleY of 2 will stretch it across the 
+         * bottom of the game.
+         */
+        createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
+
+        // example:
+        createPlatform(450, 300, 0.75);
+        createPlatform(150, 400, 0.5);
+        createPlatform(420, 510, 0.5);           // normal platform
+        createPlatform(200, 150, 0.3)       // small horizontal platform (30% the normal width)
+        createPlatform(240, 590, 0.3, 2.5)   // tall vertical wall (30% the normal width and 10x the height)
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    }
+    platform.init = init;
+})(window);
